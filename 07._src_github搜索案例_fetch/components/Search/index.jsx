bfcf021c40e3afa265bca04de56bbe08eecad593
Zjl-u <2373007562@qq.com>
2026-02-03 14:53:10 +0800

    Add files via upload

---
+++ 07._src_github搜索案例_fetch/components/Search/index.jsx
@@ -0,0 +1,54 @@
+import React, { Component } from "react"
+import PubSub from "pubsub-js"
+import axios from "axios"
+
+export default class Search extends Component {
+    search = async () => {
+        // 获取用户输入(连续解构赋值+重命名)
+        const { keyWordElement: { value: keyWord } } = this
+        // 发送请求前通知List更新状态
+        PubSub.publish('chuan', { isFirst: false, isLoading: true })
+
+        // 发送网络请求-使用fetch发送-未优化
+        // fetch(`/api1/search/user2?q=${keyWord}`).then(
+        //     response => {
+        //         console.log('联系服务器成功了',);
+        //         return response.json()
+        //     },
+        //     error => {
+        //         console.log('联系服务器失败了', error);
+        //         return new Promise(() => { })
+        //     }
+        // ).then(
+        //     response => {
+        //         console.log('获取数据成功了', response);
+        //     },
+        //     error => {
+        //         console.log('获取数据失败了', error);
+        //     }
+        // )
+        // }
+
+        // 发送网络请求-使用fetch发送-优化
+        try {
+            const response = await fetch(`/api1/search/user2?q=${keyWord}`)
+            const data = await response.json()
+            PubSub.publish('chuan', { isLoading: false, users: data.items })
+        } catch (error) {
+            PubSub.publish('chuan', { isLoading: false, err: error.message })
+        }
+    }
+
+
+    render() {
+        return (
+            <section className='jumbotron'>
+                <h3 className='jumbotron-heading'>搜索github用户</h3>
+                <div>
+                    <input ref={c => this.keyWordElement = c} type="text" placeholder='输入关键词点击搜索' />&nbsp;
+                    <button onClick={this.search}>搜索</button>
+                </div>
+            </section>
+        )
+    }
+}
